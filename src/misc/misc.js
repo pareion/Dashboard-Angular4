e36d63bc3bca520edd5ed7518a3f87d2fb4fd8ee
Sam Møller <samoe@odense.dk>
2017-08-22 09:03:16 +0200

    Fix JQuery error in AdminLE module

---
+++ src/misc/misc.js
@@ -0,0 +1,40 @@
+$(function () {
+    "use strict";
+
+    //Make the dashboard widgets sortable Using jquery UI
+    $(".connectedSortable").sortable({
+        placeholder: "sort-highlight",
+        connectWith: ".connectedSortable",
+        handle: ".box-header, .nav-tabs",
+        forcePlaceholderSize: true,
+        zIndex: 999999
+        });
+    $(".connectedSortable .box-header, .connectedSortable .nav-tabs-custom").css("cursor", "move");
+
+    //jQuery UI sortable for the todo list
+    $(".todo-list").sortable({
+        placeholder: "sort-highlight",
+        handle: ".handle",
+        forcePlaceholderSize: true,
+        zIndex: 999999
+    });
+    
+    //Fix for charts under tabs
+    $('.box ul.nav a').on('shown.bs.tab', function () {
+        area.redraw();
+        donut.redraw();
+        line.redraw();
+    });
+
+
+    $(".todo-list").todolist({
+    onCheck: function (ele) {
+        window.console.log("The element has been checked");
+        return ele;
+    },
+    onUncheck: function (ele) {
+        window.console.log("The element has been unchecked");
+        return ele;
+    }
+    });
+});
