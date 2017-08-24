3783a957bdb5244f7fca0ae4635bad801f05e636
Jeppe Troels Ærenlund <jeppe.aerenlund@gmail.com>
2017-08-24 11:02:13 +0200

    Refactored Remove Widget Functionality

--- src/app/components/widgetarea/widgetarea.component.ts
+++ src/app/components/widgetarea/widgetarea.component.ts
@@ -1,3 +1,4 @@
+import { element } from 'protractor';
 import { Component, Input, AfterViewInit, ViewChild, ComponentFactoryResolver, OnDestroy  } from '@angular/core';
 import { WidgetItem } from './../../services/widgetlibrary-service/widget-item';
 import { WidgetComponent } from './../../services/widgetLibrary-service/widget.component';
@@ -11,11 +12,13 @@ import { WidgetLibraryService } from '../../services/widgetLibrary-service/widge
 })
 export class WidgetareaComponent {
   @ViewChild(WidgetHostDirective) widgetHost: WidgetHostDirective;
+  activeWidgets: number[]; //Contains ID's of Widgets
 
   constructor(
     private widgetService: WidgetLibraryService, 
     private componentFactoryResolver: ComponentFactoryResolver) {
     
+    this.activeWidgets = [];
     this.widgetService.spawn = () => this.spawnWidget(); //Subscribe to event
     this.widgetService.remove = () => this.removeWidget(); //Subscribe to event
   }
@@ -32,22 +35,39 @@ export class WidgetareaComponent {
     (<WidgetComponent>componentRef.instance).id = this.widgetService.widgetsToBeSpawned[index].id;
     (<WidgetComponent>componentRef.instance).title = this.widgetService.widgetsToBeSpawned[index].title;
 
+    //Add to activewidgets list
+    this.activeWidgets.push(this.widgetService.widgetsToBeSpawned[index].id);
     //Done - Remove from list.
     this.widgetService.widgetsToBeSpawned.splice(index, 1);
     }
   }
 
-  //Subscriber method for removing the standard widget into DOM based on ID.
-  //Typical JavaScript implementation.
+  //To do: Comments
   removeWidget() {
     for (var index = 0; index < this.widgetService.widgetsToBeRemoved.length; index++) {
-      //Find widget
-      var elementId = this.widgetService.widgetsToBeRemoved[index].id.toString();
-      var widgetRemove = document.getElementById(elementId);
-      //remove from document
-      widgetRemove.remove();
+      
+      //Find widget index on active array list
+      let activeWidgetIndex: number;
+      for (var index2 = 0; index2 < this.activeWidgets.length; index2++) {
+        if(this.activeWidgets[index2] == this.widgetService.widgetsToBeRemoved[index].id){
+          activeWidgetIndex = index2;
+        }  
+      }
+
+      let componentFactory = this.componentFactoryResolver.resolveComponentFactory(this.widgetService.widgetsToBeRemoved[index].component);
+      let viewContainerRef = this.widgetHost.viewContainerRef;
+      viewContainerRef.remove(activeWidgetIndex);
+      
+      //Remove from list
+      this.activeWidgets.splice(activeWidgetIndex, 1);
       //Done - remove the widget from the array
       this.widgetService.widgetsToBeRemoved.splice(index, 1);
     }
   }
 }
+
+      //Find widget
+      //var elementId = this.widgetService.widgetsToBeRemoved[index].id.toString();
+      //var widgetRemove = document.getElementById(elementId);
+      //remove from document
+      //widgetRemove.remove();
